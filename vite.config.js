256e4ab95c3e752fb544317bd9cdc0d628d4a7ec
Rahul Kujur <rahulkujur31@gmail.com>
2024-10-07 23:13:45 +0530

    last commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './',
-})
+export default defineConfig(({ mode }) => {
+  let basePath = './'; // Default base path for development
+
+  // Check if it's production
+  if (mode === 'production') {
+    basePath = './'; // Set your production base path here
+  }
+
+  // You can add more logic if needed for other environments like staging, testing, etc.
+
+  return {
+    plugins: [react()],
+    base: basePath, // Apply the appropriate base path based on mode
+  };
+});
