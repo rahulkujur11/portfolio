bd46b8b3f42c1fe9b2282f8ebc9dd70f208dd784
Rahul Kujur <rahulkujur31@gmail.com>
2024-10-07 18:28:48 +0530

    last commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // Adjust the base path if needed
+  build: {
+    outDir: 'dist', // Default is dist, ensure this is correct
+  },
   plugins: [react()],
 })
